f1595bbade274019373893aa5e69787a5c03e6a8
root <root@ip-172-31-35-209.ap-northeast-2.compute.internal>
2020-06-29 19:33:52 +0900

    add videojs

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const http = require('http'),
+    fs = require('fs');
+
+let server = http.createServer(function (req, res) {
+
+    // HTML 파일 읽기
+    fs.readFile('views/index.html', function (error, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(data);
+    });
+}).listen(3000, function () {
+    console.log(`Server Running at http://localhost:3000`);
+});
+
+// let io = socketio.listen(server);
+// io.sockets.on('connect', function (socket) {
+
+//     socket.on('message', (data) => {
+//         io.sockets.emit('message', data);
+//     });
+// });
